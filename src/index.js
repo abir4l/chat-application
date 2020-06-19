ecd76d40dda11362aee76e3960b8616df38f2685
Abiral Khanal <abiralkhanal@outlook.com>
2020-06-19 13:22:56 +0900

    docker with env

--- src/index.js
+++ src/index.js
@@ -1,24 +1,15 @@
 const ex = require('express')
 const fs = require('fs') 
 const mongo = require('mongodb').MongoClient
-const url = 'mongodb://db:27017'
-
-
+const url = 'mongodb://db:' + process.env.MONGO_PORT
 
 
 //to be changed to a library reading env filee
-data = fs.readFileSync('.env',{ encoding:'utf-8',flag:'r' })
-env = data.split('\n').filter(data=>data.indexOf('=') >= 0).reduce((sum,val)=>{
-	arr = val.split('=');
-	sum[arr[0]] = arr[1];
-	return sum;
-},{});
 
 const app = ex();
 
 app.get('/mongo',(req,res) => {
 
-	console.log(url);
 	mongo.connect(url, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true
@@ -43,6 +34,6 @@ app.get('/',(req,res) => {
 	res.send('hello world')
 });
 
-app.listen(env.NODE_PORT,'0.0.0.0',() => {
+app.listen(process.env.NODE_PORT,'0.0.0.0',() => {
 	console.log('started')
 })
