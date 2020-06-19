cd251fa89a90d6b6097d3d2941fb0bbcad6d709f
Abiral Khanal <abiralkhanal@outlook.com>
2020-06-19 16:05:57 +0900

    - mongo database test success

--- src/index.js
+++ src/index.js
@@ -1,30 +1,44 @@
 const ex = require('express')
 const fs = require('fs') 
-const mongo = require('mongodb').MongoClient
-const url = 'mongodb://db:' + process.env.MONGO_PORT
+const { Connection } = require('./db.js')
+const bodyParser = require('body-parser')
 
 
 //to be changed to a library reading env filee
 
 const app = ex();
+app.use(bodyParser.json())
 
 app.get('/mongo',(req,res) => {
 
-	mongo.connect(url, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true
-}, (err, client) => {
-	if (err) {
-		console.error('error')
-	}
-	else
-		console.log('connected to mongo')
+	Connection.connect()
+	.then(db =>{
+			doc = db.db('chat');
+			doc.collection('testData').find({}).toArray((err,result) =>{
+				res.send(result)
 	});
-
-	res.send('mongo checking')
+	}).catch(err =>{
+			console.log(err)
+	});
 
 });
 
+app.post('/post-check',(req,res)=>{
+	Connection.connect()
+	.then(db =>{
+			doc = db.db('chat');
+			obj = {'source':'api',data: req.body.data}
+			doc.collection('testData')
+			.insertOne(obj,(err,res)=>{
+				if(err) throw err;
+				console.log('inserted to database')
+			});
+	}).catch(err => {
+			console.log(err);
+	});
+	res.send(req.body.data);
+
+});
 app.get('/check',(req,res) => {
 	res.send('route added')
 });
