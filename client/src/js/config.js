ffbd28181b94fef5698ce3072b327aab8dc5ee7f
Abiral Khanal <abiral09@gmail.com>
2021-02-03 21:46:10 +0900

    - config changed with url

--- client/src/js/config.js
+++ client/src/js/config.js
@@ -1,5 +1,5 @@
 //const serverUrl = 'http://localhost:8888/';
-const serverUrl = 'https://f52de4033382.ngrok.io/';
+const serverUrl = 'https://chat.abiralkhanal.com:8888/';
 export default {
    
     url: (path) =>serverUrl + path
