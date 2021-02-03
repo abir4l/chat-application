70ea021c9f293a46285faf4d973f0569aefa4966
Abiral Khanal <abiral09@gmail.com>
2021-02-03 21:49:32 +0900

    config url

--- client/src/js/config.js
+++ client/src/js/config.js
@@ -1,5 +1,5 @@
 //const serverUrl = 'http://localhost:8888/';
-const serverUrl = 'https://chat.abiralkhanal.com:8888/';
+const serverUrl = 'http://chat.abiralkhanal.com:8888/';
 export default {
    
     url: (path) =>serverUrl + path
